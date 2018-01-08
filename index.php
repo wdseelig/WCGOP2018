c3e174f9fde738183dffd6ce7fa96879a80a4de9
wdseelig <wdseelig@gmail.com>
2018-01-08 16:15:03 -0500

    Minor changes to accommodate our eventual migration to php 7.  Did not include the new php.ini file, however, because this will not work until we convert our server over to the "swift" package.

--- index.php
+++ index.php
@@ -15,7 +15,10 @@
  * Root directory of Drupal installation.
  */
 define('DRUPAL_ROOT', getcwd());
-
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+error_reporting(E_ALL);
+$myvalue = variable_get('chosen_jquery_selector');
+$y=1;
+//ini_set('display_errors',1);
 menu_execute_active_handler();
