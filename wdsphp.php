558ef722a83a303e705a38fb54fde890f01d74ca
wdseelig <wdseelig@gmail.com>
2018-11-11 10:42:12 -0500

    November 2018 commit

--- wdsphp.php
+++ wdsphp.php
@@ -1,9 +1,18 @@
 <?php
-printf("\n\nhello world!");
+$arr = array();
+if (empty($arr)){
+    printf("The array was seen as empty");
+}
+else {
+    printf("The array was seen as not empty");
+}
+printf("\n\nhello world! I am in the wdsphp file at www.wash-gop.com and am experimenting on the Swift system (I think)");
+echo exec('whoami');
+printf("\n\nThat was who apache is running as, I think<br><br>");
 phpinfo();
 
 $tstmp = 1441044069;
 print ("<br><br>The timestamp is:  " . $tstmp);
 print ("<br><br>And the date of that timestamp is:  " . date(' M/d/Y h:i A', $tstmp));
-
+//	This is just  comment.
 ?>
